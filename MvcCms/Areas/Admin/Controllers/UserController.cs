c46aa296099f3232795568572454edc9fc38a6f3
serge@serge.by <serge@serge.com>
2017-05-27 18:28:25 +0300

    NW - User Management (tbc)

---
+++ MvcCms/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCms.Areas.Admin.Controllers
+{
+    [RouteArea("admin")]
+    [RoutePrefix("user")]
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var manager = new Data.CmsUserManager())
+            {
+                var users = manager.Users.ToList();
+                return View(users);
+            }
+        }
+    }
+}
